ec284395a5a93903bab11bb1e742d2aac2f4f176
carojansing <c.jansing@basecom.de>
2023-05-10 14:50:25 +0200

    changed

---
+++ register.php
@@ -0,0 +1,36 @@
+<?php
+// logic to handle user sign up and login
+?>
+<html>
+<head>
+    <script src="https://kit.fontawesome.com/60f1874be9.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="styles/login.css">
+    <title>Car Park Homepage</title>
+</head>
+<body>
+<div class="login-box">
+    <h2>Register</h2>
+    <form>
+        <div class="user-box">
+            <input type="text">
+            <label>Username</label>
+        </div>
+        <div class="user-box">
+            <input type="password">
+            <label>Password</label>
+        </div>
+        <div class="user-box">
+            <input type="password">
+            <label>Repeat Password</label>
+        </div>
+        <a href="#">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            Submit
+        </a>
+    </form>
+</div>
+</body>
+</html>
