2d5c64f6dae82af1acc89b6e2a7cacc215210133
Alexander Miller <a.miller@basecom.de>
2023-05-31 14:45:30 +0200

    add database logic

--- src/Controller/ParkhausController.php
+++ src/Controller/ParkhausController.php
@@ -2,15 +2,27 @@
 
 namespace App\Controller;
 
+use Doctrine\DBAL\Connection;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ParkhausController extends AbstractController
 {
+
+    private $connection;
+
+    public function __construct(Connection $connection){
+        $this->connection = $connection;
+    }
+
     #[Route('/', name: 'app_homepage')]
     public function homepageShow(): Response
     {
+        $query = 'SELECT * FROM test';
+
+        $results = $this->connection->executeQuery($query)->fetchAll();
+
         return $this->render('pages/home.html.twig');
     }
 }
